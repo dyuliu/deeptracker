a220506bef53c40c09f4dc6c1acabc837c3d81a0
Dongyu Liu <Dongyu Liu>
2017-02-21 09:11:24 +0800

    gup

--- src/client/visualization/controllers/layer.client.controller.ts
+++ src/client/visualization/controllers/layer.client.controller.ts
@@ -5,6 +5,7 @@ namespace application {
   interface IScope extends ng.IScope {
     data: any[];
     open: any;
+    selectedLayers: any[];
   }
 
   class Controller {
@@ -20,16 +21,30 @@ namespace application {
       $http: ng.IHttpService
     ) {
       let this_ = this;
+      $scope.selectedLayers = [];
 
-      console.log('fetching tree.json');
       $q.all([
         DataManager.fetchInfo({ db: Global.getSelectedDB(), type: 'layer' }),
         $http.get('/json/tree.json')
-      ]).then( (data: any) => {
+      ]).then((data: any) => {
+        for (let o of data[0]) {
+          $scope.selectedLayers.push(o.lid);
+        }
         data[1] = data[1].data;
         for (let d of data[1]) { d.opened = false; }
         $scope.data = data;
       });
+
+      let opt: IHTTPOptionConfig = {
+        db: Global.getSelectedDB(),
+        type: 'w_mean',
+        layer: $scope.selectedLayers,
+        parser: 'json'
+      };
+      DataManager.fetchLayer(opt, false)
+        .then((data: ILayerEle[]) => {
+          console.log('fetch layer', data);
+        });
 
       $('#widget-container-layerinfo')
         .mouseenter(function () {
@@ -39,25 +54,25 @@ namespace application {
           $('#widget-container-layerinfo .widget-header').addClass('invisible');
         });
 
-      $('#collapse-animation-container').click(function (e) {
-        let content = $('#collapse-animation-content');
-        if (content.css('height') === '0px') {
-          $('#collapse-animation-content')
-            .css('height', 100)
-            .css('background-color', '#dc5151');
-          $('#collapse-animation-content>div')
-            .css('display', 'block');
-        } else {
-          $('#collapse-animation-content')
-            .css('height', 0)
-            .css('background-color', 'white');
-          $('#collapse-animation-content>div')
-            .css('display', 'none');
-        }
+      // $('#collapse-animation-container').click(function (e) {
+      //   let content = $('#collapse-animation-content');
+      //   if (content.css('height') === '0px') {
+      //     $('#collapse-animation-content')
+      //       .css('height', 100)
+      //       .css('background-color', '#dc5151');
+      //     $('#collapse-animation-content>div')
+      //       .css('display', 'block');
+      //   } else {
+      //     $('#collapse-animation-content')
+      //       .css('height', 0)
+      //       .css('background-color', 'white');
+      //     $('#collapse-animation-content>div')
+      //       .css('display', 'none');
+      //   }
 
-      });
+      // });
 
-      $scope.open = function(d, level) {
+      $scope.open = function (d, level) {
         console.log($('#' + d.name + ' .level2'));
         $('#' + d.name + ' .level2')
           .removeClass('no-disp')
